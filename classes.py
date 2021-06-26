2f57eda5431d5bf3a035e73a6ff15901cb0c19f5
Alexfuze <83480360+Alexfuze@users.noreply.github.com>
2021-06-26 19:06:21 -0400

    Delete classes.py

--- classes.py
+++
@@ -1,7 +0,0 @@
-class Classes:
-
-    def __init__(self, name, attack, vie, possibilite_arme):
-        self.name = name
-        self.attack = attack
-        self.vie = vie
-        self.possibilite_arme = possibilite_arme
